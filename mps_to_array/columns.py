82a149352e39058b92faa8ea349dd0569fcb5d7c
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-09-01 12:03:46 +0300

    Update columns.py

--- mps_to_array/columns.py
+++ mps_to_array/columns.py
@@ -1,4 +1,4 @@
-def getColumns(list):
+def getColumns(lista):
     """ Adds the columns  to a list
     Args:
         A list.
@@ -7,13 +7,13 @@ def getColumns(list):
     """
     columns = []
     cls = False
-    for i in list:
-        if (str(i[0:7]).upper() == "COLUMNS"):
+    for i in lista:
+        if str(i[0:7]).upper() == "COLUMNS":
             cls = True
             continue
         if str(i[0:3]).upper() == "RHS":
             break
-        if (cls == True):
+        if cls:
             columns.append(i)
     return columns
 
@@ -24,7 +24,7 @@ def getColVarName(columnlist):
     Returns:
         A list with the name of every column. if no columns found returns an empty list.
     """
-    colvarnames= []
+    colvarnames = []
     for i in columnlist:
         colvarnames.append(i[4:12])
     return colvarnames
@@ -71,13 +71,14 @@ def getColRestrictionName2(columnlist):
     return colrestname2
 
 def getColRestrictionValue2(columnlist, objname):
-        """ Adds the values of restriction 2 to a list
+    """ Adds the values of restriction 2 to a list
     Args:
         A list with columns
         A name of a row
     Returns:
         A list with the values of restriction 2. if no restrictions 1 found returns an empty list.
     """
+
     colrestvalue2 = []
     objname = str(objname).strip("\n")
     for i in columnlist:
@@ -86,7 +87,7 @@ def getColRestrictionValue2(columnlist, objname):
         colrestvalue2.append(i[49:61])
     return colrestvalue2
 
-def getColVarNameFromRest(columnlist,restrictionname):
+def getColVarNameFromRest(columnlist, restrictionname):
     """
     Saves the variable name based on restriction name
     Args:
@@ -96,7 +97,7 @@ def getColVarNameFromRest(columnlist,restrictionname):
         A list of variable names.if none returns an empty list.
     """
     restrictionname = str(restrictionname).strip("\n")
-    colvarnamesfromobj= []
+    colvarnamesfromobj = []
     for i in columnlist:
         if str(i[39:46]).strip(" ") == str(restrictionname).strip(" ") or str(i[14:21]).strip(" ") == (restrictionname).strip(" "):
             colvarnamesfromobj.append(i[4:12])
